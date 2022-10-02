e07be5ff1e11ed56b3b76eabc119c1b8e68a8e51
Beknazar Abdyldaev <abdyldaev.duff@gmail.com>
2022-10-02 14:50:30 +0600

    add testing functional for converter

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,15 +1,22 @@
+import requests
+
 from django.shortcuts import render
 
 from mainapp.forms import ConverterForm
 
 
 def index(request):
+    url = 'https://api.exchangerate-api.com/v4/latest/USD'
+    response = requests.get(url).json()
+    currencies = response.get('rates')
+    
     if request.method == 'POST':
         form = ConverterForm(request.POST)
     else:
         form = ConverterForm()
     context = {
-        'form': form
+        'form': form,
+        'currencies': currencies,
     }
     return render(request, 'mainapp/index.html', context)
 
