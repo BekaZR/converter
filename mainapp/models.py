d15cbacc3e78f148cee55394914739789d0f5435
Beknazar Abdyldaev <abdyldaev.duff@gmail.com>
2022-10-02 14:15:40 +0600

    add Converter model

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Converter(models.Model):
+    amount = models.PositiveIntegerField(default=0, verbose_name='Сумма перевода')
+    sale_currency = models.CharField(
+        max_length=50, verbose_name='Продажа валюты'
+    )
+    buy_currency = models.CharField(
+        max_length=50, verbose_name='Купля валюты'
+    )
+    get_amount = models.PositiveIntegerField(default=0, verbose_name='Мы получаем')
+    
+    def __str__(self):
+        return f'{self.id}'
+    
+    
+    class Meta:
+        verbose_name = "Конвертер валюты"
+        verbose_name = "Конвертер валют"
+        
